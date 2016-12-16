80cb6fd79c09686edac421b968f6992ecfbc30b7
TwelveLab12 <bruno.schvartz@gmail.com>
2016-12-16 10:57:50 +0100

    Cls route web

--- twelve/routes/web.php
+++ twelve/routes/web.php
@@ -11,38 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('salut', function () {
-    return 'Salut les gens';
-});
-
-Route::get('salut/{slug}-{id}',['as' => 'salut', function ($slug, $id) {
-    return 'Lien: ' . route('salut', ['slug' => $slug, 'id' => $id]);
-}])->where('slug', '[a-z0-9\-]+')->where('id','[0-9]+');
-
-Route::get('a-propos',['as' => 'about', 'uses' => 'PageController@about'] );
-
-Route::resource('welcome', 'WelcomeController', ['only' => ['index']]);
+Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+    
 Route::resource('article', 'PostsController');
 Route::resource('categorie', 'CategoriesController');
 Route::resource('tag', 'TagsController');
 
+Route::get('a-propos',['as' => 'about', 'uses' => 'PageController@about'] );
 Auth::routes();
-
-Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
-    Route::get('/', function () {
-        return 'welcome';
-    });
+Route::group(['prefix' => 'cockpit', 'middleware' => ['auth']], function(){
+    
 });
 
-Route::resource('link','LinksController', ['only' => ['create', 'store']]);
-Route::get('r/{link}', ['as' => 'link.show', 'uses' => 'LinksController@show'])->where('link', '[0-9]+');
-//Route::group(['prefix' => 'links'], function(){
-//    Route::get('/create', ['as' => 'createLinks', 'uses' => 'LinksController@create']);
-//});
 
-Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
+
+
+
 
